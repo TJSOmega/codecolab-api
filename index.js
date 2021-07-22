cccd88213fece0ecc4b9207119df1532dcd80e1d
Terrence Jamal Simms <69491583+TJSOmega@users.noreply.github.com>
2021-07-22 00:22:21 -0700

    Condensed down server

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
+const express = require('express');
 require('dotenv').config();
+const cors = require('cors')
 const mongoose = require('mongoose');
-const io = require('socket.io')(3005)
+const http = require('http');
+const socketio = require('socket.io')
 
 
-const server = require('./src/server.js')
-
-
-const PORT = process.env.PORT || 3333;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/codecolab';
 const options = { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true };
 
+const port = process.env.PORT || 3333
 
-io.on("connection", () => {
-  console.log('App connected')
-});
+const v1Routes = require('./src/routes/v1.js');
+
+const errorHandler = require('./src/error-handlers/500.js');
+
+const notFound = require('./src/error-handlers/404.js');
+
+const logger = require('./src/middleware/logger.js');
+
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.use(logger)
+app.use(v1Routes);
+app.use(notFound);
+app.use(errorHandler);
+
+io.on('connection', socket => {
+  console.log(socket.id)
+  socket.send('Hello and welcome to Code Co Lab!')
+})
 
 mongoose.connect(MONGODB_URI, options)
   .then(
-    server.start(PORT)
-
+    server.listen(port, () => console.log(`Now listening on port ${port}.`))
   )
-
- 
-  
