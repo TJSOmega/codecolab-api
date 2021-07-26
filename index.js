dfc552004727963f02c5d7a65ca8f86446e9b105
Terrence Jamal Simms <69491583+TJSOmega@users.noreply.github.com>
2021-07-26 15:11:41 -0700

    Trying to fix chat feature

--- index.js
+++ index.js
@@ -114,6 +114,7 @@ io.on('connection', socket => {
       console.log('MESSAGE', message)
 
       users.forEach(u => {
+        console.log('user message')
         if (u.user_id === socket.id) {
           console.log("USER ROOM", u.room)
           io.to(u.room).emit('message', { user: u.user_name, text: message });
